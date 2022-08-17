326bb5d40bf411ed90a6d218127eb689f34e4736
RokuFSD <emilianordx@gmail.com>
2022-08-16 21:13:23 -0300

    home page desktop style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,19 @@ module.exports = {
         160: '40rem',
         176: '44rem',
         256: '64rem'
+      },
+      keyframes: {
+        'slide-in-left': {
+          from: {
+            transform: 'translateX(-50%)'
+          },
+          to: {
+            transform: 'translateX(0)'
+          }
+        }
+      },
+      animation:{
+        'slide-in-left': 'slide-in-left 0.5s ease-in-out'
       }
     }
   },
