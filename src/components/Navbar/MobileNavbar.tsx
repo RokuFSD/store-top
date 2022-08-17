b7850d600a0d84775ff9656b4d41df3f2a99beb1
RokuFSD <emilianordx@gmail.com>
2022-08-17 17:27:10 -0300

    Add hamburger state

--- src/components/Navbar/MobileNavbar.tsx
+++ src/components/Navbar/MobileNavbar.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import HamburgerSvg from '../Svg/HamburgerSvg';
 
 function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <HamburgerSvg />
-      <nav className="hidden">
+      <button type="button" onClick={handleClick}>
+        <HamburgerSvg />
+      </button>
+      <nav className={`${isOpen ? 'visible' : 'hidden'}`}>
         <ul className="flex gap-12">
           <Link to="/" className="navlink">
             home
