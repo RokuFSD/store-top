4c2800a59d3b8953f13e7899103e51b7d04df4a0
RokuFSD <emilianordx@gmail.com>
2022-08-18 19:56:14 -0300

    Fix css on the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-      <ul className="flex gap-12">
+      <ul className="bg-white border-t-2 h-36 flex flex-col w-full gap-4 justify-center items-center lg:flex-row lg:gap-12 lg:h-auto lg:border-none">
         <Link to="/" className="navlink">
           home
         </Link>
