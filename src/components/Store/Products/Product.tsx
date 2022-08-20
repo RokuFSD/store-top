ba38fcfc0c019dead95dd232343461b0708183d7
RokuFSD <emilianordx@gmail.com>
2022-08-19 23:07:35 -0300

    Styles for product component

--- src/components/Store/Products/Product.tsx
+++ src/components/Store/Products/Product.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IProduct } from '../../../types/api';
+import PlusSvg from '../../Svg/PlusSvg';
 
 type ProductProps = {
   product: IProduct;
@@ -7,17 +8,21 @@ type ProductProps = {
 
 function Product({ product }: ProductProps) {
   return (
-    <div className="flex flex-col w-64 overflow-hidden gap-2">
-      <div className="bg-white">
-        <img src={product.images[0]} alt="product" className="w-4/6 mx-auto" />
-      </div>
-      <h2 className="font-semibold">
+    <div className="flex flex-col w-11/12 overflow-hidden gap-3 card md:w-72">
+      <img src={product.images[0]} alt="product" />
+      <h2 className="font-semibold px-1">
         {product.title}
         <span className="block line-clamp-2 font-normal text-xs w-44">{product.description}</span>
       </h2>
-      <div className="w-full flex justify-between pr-8">
-        <span>{product.price}</span>
-        <button type="button">Add to cart</button>
+      <div className="w-full flex justify-between items-center px-1 my-1">
+        <span className="text-md">$ {product.price}</span>
+        <button
+          type="button"
+          className="flex items-center gap-2 bg-neutral-800 text-white rounded-lg p-1"
+        >
+          <PlusSvg />
+          <span className="text-sm">Add to cart</span>
+        </button>
       </div>
     </div>
   );
--- src/components/Store/Products/ProductsList.tsx
+++ src/components/Store/Products/ProductsList.tsx
@@ -4,7 +4,7 @@ import Product from "./Product"
 
 function ProductsList({ products }: { products: IProduct[] }) {
   return (
-    <section className="grid place-content-center">
+    <section className="grid max-w-7xl mx-auto place-content-center py-5 place-items-center gap-8 md:grid-cols-2 lg:grid-cols-3">
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
