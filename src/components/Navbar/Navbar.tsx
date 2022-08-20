cfe2e1e0865dba926155abc10fe83166fdba60e5
RokuFSD <emilianordx@gmail.com>
2022-08-19 23:14:13 -0300

    Fix navbar on tablet

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-      <ul className="bg-white border-t-2 h-36 flex flex-col w-full gap-4 justify-center items-center lg:flex-row lg:gap-12 lg:h-auto lg:border-none">
+      <ul className="bg-white border-t-2 h-36 flex flex-col w-full gap-4 justify-center items-center md:flex-row lg:gap-12 md:h-auto md:border-none">
         <Link to="/" className="navlink">
           home
         </Link>
