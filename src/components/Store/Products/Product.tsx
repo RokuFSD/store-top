6bbe74182e885f2fd2dcf9e60e028d3accfc2aa5
RokuFSD <emilianordx@gmail.com>
2022-08-19 22:00:33 -0300

    Add tailwind line-clamp plugin

--- src/components/Store/Products/Product.tsx
+++ src/components/Store/Products/Product.tsx
@@ -3,15 +3,24 @@ import { IProduct } from '../../../types/api';
 
 type ProductProps = {
   product: IProduct;
-}
+};
 
 function Product({ product }: ProductProps) {
   return (
-    <div>
-      <h2>{product.title}</h2>
+    <div className="flex flex-col w-64 overflow-hidden gap-2">
+      <div className="bg-white">
+        <img src={product.images[0]} alt="product" className="w-4/6 mx-auto" />
+      </div>
+      <h2 className="font-semibold">
+        {product.title}
+        <span className="block line-clamp-2 font-normal text-xs w-44">{product.description}</span>
+      </h2>
+      <div className="w-full flex justify-between pr-8">
+        <span>{product.price}</span>
+        <button type="button">Add to cart</button>
+      </div>
     </div>
   );
 }
 
 export default Product;
-
--- src/components/Store/Products/ProductsList.tsx
+++ src/components/Store/Products/ProductsList.tsx
@@ -4,7 +4,7 @@ import Product from "./Product"
 
 function ProductsList({ products }: { products: IProduct[] }) {
   return (
-    <section>
+    <section className="grid place-content-center">
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
