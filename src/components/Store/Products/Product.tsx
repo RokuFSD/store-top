ba57b4d117cd5344a93f518fe3564c00ba7d619f
RokuFSD <emilianordx@gmail.com>
2022-08-25 22:35:45 -0300

    Change Api source

--- src/components/Store/Products/Product.tsx
+++ src/components/Store/Products/Product.tsx
@@ -14,23 +14,25 @@ function Product({ product }: ProductProps) {
   function addToCart() {
     dispatch?.({
       type: 'ADD_TO_CART',
-      payload: { ...product, name: product.title, quantity: 1, image: product.images[0] },
+      // eslint-disable-next-line no-underscore-dangle
+      payload: { id: +product._id, name: product.title, quantity: 1, ...product },
     });
   }
 
   return (
     <div className="flex flex-col overflow-hidden gap-3 w-64 card md:w-60">
-      <img src={product.images[0]} alt="product" />
+      <img src={product.image} alt="product" />
       <h2 className="font-semibold px-1 h-20">
         {product.title}
         <span className="block line-clamp-2 font-normal text-xs w-44">{product.description}</span>
       </h2>
       <div className="w-full flex justify-between items-center px-1 my-1">
         <span className="text-md">$ {product.price}</span>
-        <ProductButton onClick={() => addToCart()} disabled={cart?.ids[product.id]} />
+        {/* eslint-disable-next-line no-underscore-dangle */}
+        <ProductButton onClick={() => addToCart()} disabled={cart?.ids[+product._id]} />
       </div>
     </div>
   );
 }
 
-export default Product;
+export default React.memo(Product);
--- src/components/Store/Products/ProductsList.tsx
+++ src/components/Store/Products/ProductsList.tsx
@@ -1,32 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Product from './Product';
 import { IProduct } from '../../../types/api';
-import useFetch from '../../../hooks/useFetch';
 
-const URL = 'https://api.escuelajs.co/api/v1';
+type ProductsListProps = {
+  products: IProduct[];
+};
 
-function ProductsList() {
+function ProductsList({ products }: ProductsListProps) {
   const { id } = useParams();
-  const { data:products, error} = useFetch<IProduct[]>(id ? `${URL}/categories/${id}/products` : `${URL}/products`);
+  const filteredProducts = id
+    ? products.filter((product) => product.category.slug === id)
+    : products;
 
-  if (error) {
-    return <div>There is an error</div>;
-  }
   return (
     <div className="productsList">
-      {!products ? (
-        <div>Loading...</div>
-      ) : (
-        <section className="grid gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
-        </section>
-      )}
+      <section className="grid gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {filteredProducts?.map((product) => (
+          // eslint-disable-next-line no-underscore-dangle
+          <Product key={product._id} product={product} />
+        ))}
+      </section>
     </div>
   );
 }
 
 export default ProductsList;
-
