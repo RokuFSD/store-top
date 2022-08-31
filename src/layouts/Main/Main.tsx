7b0a984dce7cfe0ea7794a9c54618375721cfbc0
RokuFSD <emilianordx@gmail.com>
2022-08-31 18:45:16 -0300

    Fix main section style

--- src/layouts/Main/Main.tsx
+++ src/layouts/Main/Main.tsx
@@ -1,11 +1,7 @@
 import React, { ReactNode } from 'react';
-import { useLocation } from 'react-router-dom';
-
-const pathnames = ['/', '/cart'];
 
 function Main({ children }: { children: ReactNode }) {
-  const { pathname } = useLocation();
-  return <main className={`${pathnames.includes(pathname) ? 'main' : 'min-h-screen'}`}>{children}</main>;
+  return <main className="main">{children}</main>;
 }
 
 export default Main;
