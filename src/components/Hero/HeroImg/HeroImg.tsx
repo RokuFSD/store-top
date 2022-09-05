102d9e5fb90d574f4202b3588a9332c912d2f433
RokuFSD <emilianordx@gmail.com>
2022-09-05 17:36:30 -0300

    Change to hash router for gh-pages deploy

--- src/components/Hero/HeroImg/HeroImg.tsx
+++ src/components/Hero/HeroImg/HeroImg.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function HeroImg() {
   return (
     <img
-      src="src/assets/thrift-shop-not-css.svg"
+      src="./"
       alt="thrift shop"
       className="w-full max-w-2xl order-1 lg:order-2 lg:max-w-lg 2xl:max-w-3xl"
     />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: '/store-top/',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'assets/images/[name]-[hash][extname]';
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
+});
